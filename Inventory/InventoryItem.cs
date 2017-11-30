61bf54c354d66beccad742b8d56752d2549fccf0
gjansen <geert__jansen@hotmail.com>
2017-11-30 11:06:27 +0100

    Add project files.

---
+++ Inventory/InventoryItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory {
+    public class InventoryItem {
+        public string Description { get; set; }
+        public int Ammount { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
---
+++ Inventory/InventoryStore.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class InventoryStore
+    {
+        private List<InventoryItem> LocalInventory = new List<InventoryItem>();
+
+        public InventoryStore() {
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 3, Price = 33.95M, Description = "Pencil" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 1, Price = 00.39M, Description = "Notebook" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+            this.LocalInventory.Add(new InventoryItem() { Ammount = 9, Price = 15.00M, Description = "Whiteboard marker" });
+        }
+
+        public List<InventoryItem> GetInventory() {
+            return this.LocalInventory;
+        }
+    }
+}
